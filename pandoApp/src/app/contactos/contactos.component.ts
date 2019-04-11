f284caa45d98a2dcb3d21b5022be919eb52a3432
singularAna <a8alumno@aules.local>
2019-04-11 16:23:25 +0200

    contactosamediohacer

--- pandoApp/src/app/contactos/contactos.component.ts
+++ pandoApp/src/app/contactos/contactos.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactosService } from 'src/app/servicios/contactos.service';
+import { Usuario } from 'src/app/modelos/usuario';
 
 @Component({
   selector: 'contactos',
   templateUrl: './contactos.component.html',
   styleUrls: ['./contactos.component.scss']
 })
+
 export class ContactosComponent implements OnInit {
-  
-  contactos = null;
+
+  usuarios = null;
 
 
-  constructor(private _) { }
+  constructor(private _contactosServ: ContactosService) { }
 
   ngOnInit() {
-    this.AppServe.geAppAPI().subscribe((data)=>{
-      this.contacto = data;
-    });
+    this.usuarios = this._contactosServ.getUsuarios();
   }
 
 }
--- pandoApp/src/app/servicios/contactos.service.ts
+++ pandoApp/src/app/servicios/contactos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { Usuario } from 'src/app/modelos/usuario';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -9,15 +10,26 @@ import { Observable } from 'rxjs/internal/Observable';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ContactosService {
 
-  constructor(private _http: HttpClient) { }
+  constructor() { }
 
-  geAppAPI():Observable<Contacto[]>{
-    return this._http.get<Contacto[]>('http://www.mocky.io/v2/5cadb0952f00000d343a96e8')
+  private _usuarios = [
+    new Usuario(23, ' Pepe', '****', 'Pepito','fotomia'),
+    new Usuario(23, ' Juan', '**********', 'Juanito','fotomia'),    
+    new Usuario(23, ' Pepe', '****', 'Pepito','fotomia'),
+    new Usuario(23, ' Juan', '**********', 'Juanito','fotomia'),    
+    new Usuario(23, ' Pepe', '****', 'Pepito','fotomia'),
+    new Usuario(23, ' Juan', '**********', 'Juanito','fotomia')
+  ];
+
+  getUsuarios() {
+    // console.log('Usuarios:', this._usuarios)
+    return this._usuarios;
   }
 
-
 }
+
 
 
