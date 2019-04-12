d348082ac90f174840ab6247255fd89aa315f488
Devilzac <shane199274@gmail.com>
2019-04-13 01:01:14 +0200

    Componente logo animado

---
+++ pandoApp/src/app/logo-animado/logo-animado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'logo-animado',
+  templateUrl: './logo-animado.component.html',
+  styleUrls: ['./logo-animado.component.scss']
+})
+export class LogoAnimadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
