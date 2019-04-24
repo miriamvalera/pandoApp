782a4920398951c456f9907f929466d704d81bbc
EC2 Default User <ec2-user@ip-172-31-0-142.eu-central-1.compute.internal>
2019-04-24 15:11:18 +0000

    cambio modelo msj

--- server/app/models/msj.js
+++ server/app/models/msj.js
@@ -6,13 +6,7 @@ const MensajesSchema   = new Schema({
     idOrig: String,
     idDest: String,
     texto: String,
-<<<<<<< HEAD
     fecha: String    
-=======
-    msgHora: String
-    
-  
->>>>>>> bdde3da91f39c98ab8e8204c0a9bffacef51105e
 });
 
 module.exports = mongoose.model('Msj', MensajesSchema);
